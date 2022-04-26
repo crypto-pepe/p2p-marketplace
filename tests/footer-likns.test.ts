57cad929987765af0acdae19602439a59fc863e8
Turbo Stump <turbo.stump@gmail.com>
2022-04-26 13:44:07 +0300

    code rafactoring

--- tests/footer-likns.test.ts
+++ tests/footer-likns.test.ts
@@ -4,33 +4,35 @@ import childProcess from 'child_process';
 const __GIT_COMMITHASH__ = childProcess.execSync('git rev-parse HEAD').toString().trim()
 
 const requiredLinks = [
-	"https://pepe.team",
-	"https://t.me/pepe_team",
-	"https://github.com/crypto-pepe",
-	"https://twitter.com/cryptopepeteam",
-	"https://crypto-pepe-team.medium.com",
-	"#",
 	`https://github.com/crypto-pepe/p2p-marketplace/tree/${__GIT_COMMITHASH__}`,
-]
+	'https://pepe.team',
+	'https://crypto-pepe-team.medium.com',
+	'https://twitter.com/cryptopepeteam',
+	'https://t.me/pepe_team',
+	'https://github.com/crypto-pepe',
+];
 
 async function getLinksArray(page) {
 	await page.goto('/');
+	const links = []
 	const footerLinks = await page.locator('footer a');
-	return await footerLinks.evaluateAll(list => list.map(link => link.getAttribute('href')));
+	console.log()
+	const count = await footerLinks.count();
+	for (let i = 0; i < count; ++i) {
+		links.push(await footerLinks.nth(i).getAttribute('href'));
+	}
+
+	return links
 }
 
-test.describe("validate footer links", async () => {
 
-	test("numbers of links", async ({ page }) => {
-		const links = await getLinksArray(page)
-		expect(requiredLinks.length === links.length).toBe(true)
-	})
 
-	test("links is equal", async ({ page }) => {
-		const links = await getLinksArray(page)
 
-		requiredLinks.forEach((requiredLink) => {
-			expect(links.includes(requiredLink)).toBe(true)
-		});
-	})
-})
+
+
+test('XXXX', async ({ page }) => {
+	await page.goto('/');
+	const linkElements = await page.locator('footer a');
+	const links = linkElements.evaluateAll(list => list.map(link => link.getAttribute('href')));
+	expect(await links).toEqual(expect.arrayContaining(requiredLinks));
+});
