6d08ce51f63b0e972896e0c197d331b566047398
Turbo Stump <turbo.stump@gmail.com>
2022-04-26 18:50:43 +0300

    refactoring

--- tests/footer-likns.test.ts
+++ tests/footer-likns.test.ts
@@ -15,6 +15,6 @@ const requiredLinks = [
 test('Footer should contain required links', async ({ page }) => {
 	await page.goto('/');
 	const linkElements = await page.locator('footer a');
-	const links = await linkElements.evaluateAll(list => list.map(link => link.getAttribute('href')));
+	const links = linkElements.evaluateAll(list => list.map(link => link.getAttribute('href')));
 	expect(await links).toEqual(expect.arrayContaining(requiredLinks));
 });
