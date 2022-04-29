df778e2b6a53fad71906194295a3baa4de2768c1
Turbo Stump <turbo.stump@gmail.com>
2022-04-29 17:50:42 +0300

    WIP redirect from account and exchanges pages without wallet connection

--- src/routes/account.svelte
+++ src/routes/account.svelte
@@ -1,11 +1,21 @@
-<script context="module">
+<script lang="ts" context="module">
 	import { browser, dev } from '$app/env';
+	import { goto } from '$app/navigation';
+	import { wallet } from '../stores/wallet';
 
 	// we don't need any JS on this page, though we'll load
 	// it in dev so that we get hot module replacement...
 	export const hydrate = dev;
 
 	export const router = browser;
+</script>
+
+<script lang="ts">
+	$: () => {
+		if (!$wallet.isConnected) {
+			goto('/');
+		}
+	};
 </script>
 
 <svelte:head>
